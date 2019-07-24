30d58504bdf25c482633a8117156c85d3bb00572
MelsTesla <89161007600@mail.ru>
2019-07-25 00:41:39 +0300

    Initial Commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by Grom Tesla on 25/07/2019.
+//  Copyright © 2019 Tesla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
